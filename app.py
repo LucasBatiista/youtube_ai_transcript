5c5665818b93341acd860722992bf3df05b758fe
Lucas Batista <lucasjsbatista@gmail.com>
2025-01-09 22:54:29 -0400

    AI ollama summarizer

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from youtube_transcript import YoutubeTranscript
+from ollama import chat
+from ollama import ChatResponse
 
 # Título da página
 st.title("Youtube Video Summarizer")
@@ -11,7 +13,24 @@ user_input = st.text_input("Video Link:")
 if st.button("Send"):
     if user_input:
         yt_transcript = YoutubeTranscript(video_link=user_input)
+        text = yt_transcript.get_transcript()
+
+        prompt = f"""You are an assistant specialized in summarizing YouTube video content clearly and concisely. Whenever you receive a text or transcript of a video, follow these instructions:
+                    Language: The summary must always be in English.
+                    Format: Use bullet points (-) to list the key points.
+                    Content: Extract only the most relevant information, keeping the summary objective and easy to understand.
+                    Structure: If the video has clear sections (such as introduction, development, and conclusion), organize the summary according to these parts.
+                    Details: Include examples, data, or important quotes if applicable.
+                    Length: The summary should have a maximum of 10 bullet points, unless the content is very extensive.
+                    Now, summarize the following video content: {text}."""
         
-        st.write(f"Transcript: {yt_transcript.get_transcript()}")
+        response: ChatResponse = chat(model='llama3.2', messages=[
+            {
+                'role': 'user',
+                'content': prompt,
+            },
+        ])
+                
+        st.write(f"{response.message.content}")
     else:
         st.write("Please paste the video link")
